cd35a87aaa187b6050c87d39bc5db199897ff037
Amirreza Nasiri <nasiri.amirreza.96@gmail.com>
2024-01-03 01:13:38 +0330

    Test edge case when domain exists

--- tests/fronthub/modifier_test.go
+++ tests/fronthub/modifier_test.go
@@ -10,26 +10,62 @@ import (
 )
 
 func Test_FronthubAddDnsZoneCommand(t *testing.T) {
-	finalFile := "/tmp/added-dns-zone.json"
-	copyFile("../assets/fronthub-empty.json", finalFile)
-	//defer deleteFile(finalFile)
+	t.Run("adds new domain", func(t *testing.T) {
+		finalFile := "/tmp/added-dns-zone.json"
+		copyFile("../assets/fronthub-empty.json", finalFile)
 
-	cmd.RootCmd.SetArgs([]string{
-		"fronthub:add-dns-zone",
-		"--config-file",
-		finalFile,
-		"--domain",
-		"my-test-domain.com",
+		defer deleteFile(finalFile)
+
+		cmd.RootCmd.SetArgs([]string{
+			"fronthub:add-dns-zone",
+			"--config-file",
+			finalFile,
+			"--domain",
+			"my-test-domain.com",
+		})
+		cmd.RootCmd.Execute()
+
+		config, err := fronthub.ReadFronthubConfig(finalFile)
+		assert.NoError(t, err)
+
+		assert.Len(t, config.Zones, 1)
+		assert.Equal(t, config.Zones[0].Domain, "my-test-domain.com")
+		assert.Empty(t, config.Zones[0].Endpoints)
+		assert.Empty(t, config.Clusters)
 	})
-	cmd.RootCmd.Execute()
 
-	config, err := fronthub.ReadFronthubConfig(finalFile)
-	assert.NoError(t, err)
+	t.Run("doesn't add the domain if already exists", func(t *testing.T) {
+		finalFile := "/tmp/added-dns-zone.json"
+		copyFile("../assets/fronthub-empty.json", finalFile)
 
-	assert.Len(t, config.Zones, 1)
-	assert.Equal(t, config.Zones[0].Domain, "my-test-domain.com")
-	assert.Empty(t, config.Zones[0].Endpoints)
-	assert.Empty(t, config.Clusters)
+		defer deleteFile(finalFile)
+
+		cmd.RootCmd.SetArgs([]string{
+			"fronthub:add-dns-zone",
+			"--config-file",
+			finalFile,
+			"--domain",
+			"my-test-domain.com",
+		})
+		cmd.RootCmd.Execute()
+
+		cmd.RootCmd.SetArgs([]string{
+			"fronthub:add-dns-zone",
+			"--config-file",
+			finalFile,
+			"--domain",
+			"my-test-domain.com",
+		})
+
+		assert.Panics(t, func() {
+			cmd.RootCmd.Execute()
+		})
+
+		config, err := fronthub.ReadFronthubConfig(finalFile)
+		assert.NoError(t, err)
+
+		assert.Len(t, config.Zones, 1)
+	})
 }
 
 func deleteFile(path string) {
